44492948e188d2631fc5a42c727a15330f57aef6
Chris Barts <chbarts@gmail.com>
2022-06-11 09:44:26 -0600

    Command line

--- youtube-on-roku.py
+++ youtube-on-roku.py
@@ -21,6 +21,12 @@ def make_roku_url(url):
     params = urllib.parse.urlencode({'t':'v', 'u':url, 'videoName':'(null)', 'k': '(null)', 'videoFormat': 'mp4'})
     return 'http://' + IP + ':8060/input/15985?' + params
 
+if len(sys.argv) != 2:
+    print('Usage: ' + sys.argv[0] + ' YOUTUBE_URL')
+    sys.exit(0)
+
+URL = sys.argv[1]
+
 ydl_opts = {'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'}
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     info = ydl.extract_info(URL, download=False)
