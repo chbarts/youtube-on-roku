431ba1bd88921ae6ff7b4406b1bab8b078be9e45
Chris Barts <chbarts@gmail.com>
2026-05-03 12:47:31 -0600

    Make pipx-compliant

---
+++ youtubeonroku.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+try:
+    import yt_dlp
+except ImportError:
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'yt-dlp')
+    if os.path.exists(path):
+        sys.path.insert(0, path)
+    elif os.path.exists(path + '.zip'):
+        sys.path.insert(0, path + '.zip')
+    else:
+        raise
+    import yt_dlp
+
+import json
+import requests
+import argparse
+import urllib.parse
+from ssdpy import SSDPClient
+import xml.etree.ElementTree as ET
+
+URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
+IP = '192.168.1.182'
+
+def pick_format(data):
+    res = None
+    for format in data['formats']:
+        if (format['ext'] == 'mp4') and (format['acodec'] != 'none') and (format['vcodec'] != 'none'):
+            res = format
+    return res
+
+def get_roku_info(roku):
+    data = requests.get(roku + 'query/device-info')
+    root = ET.fromstring(data.text)
+    loc = None
+    name = None
+    if root.find('user-device-location') != None:
+        loc = root.find('user-device-location').text
+    if root.find('user-device-name') != None:
+        name = root.find('user-device-name').text
+    elif root.find('default-device-name') != None:
+        name = root.find('default-device-name').text
+    else:
+        name = 'Unknown Roku'
+    if loc != None:
+        return "{0} ({1}) at {2}".format(name, loc, roku)
+    else:
+        return "{0} at {1}".format(name, roku)
+
+def make_roku_url(roku, url, title='(null)'):
+    params = urllib.parse.urlencode({'t':'v', 'u':url, 'videoName': title, 'k': '(null)', 'videoFormat': 'mp4'})
+    return roku + 'input/15985?' + params
+
+def get_roku():
+    client = SSDPClient()
+    devices = client.m_search('ssdp:all')
+    res = []
+    for device in devices:
+        if "roku" in device.get("usn"):
+            res = res + [device]
+    return res
+
+#        print(device.get("usn") + '\t' + device.get("location"))
+
+def main():
+    parser = argparse.ArgumentParser(description='Play YouTube videos on a Roku on the LAN.')
+
+    parser.add_argument('-n', '--number', metavar='NUMBER', nargs=1, type=int, default=0, help='Use the Roku device number NUMBER')
+    parser.add_argument('-u', '--url', metavar='ROKU_URL', nargs=1, default='', help='Use the Roku URL ROKU_URL')
+    parser.add_argument('-l', '--list', action='store_true', help='Just list the Roku devices this program can discover')
+    parser.add_argument('video', nargs='?', help='The YouTube URL or video ID')
+
+    args = parser.parse_args()
+
+    if (not args.list) and ((args.video == None) or (len(args.video) == 0)):
+        parser.print_help()
+        sys.exit(0)
+
+    if args.list:
+        rokus = get_roku()
+        n = 1
+        for roku in rokus:
+            print("{0} - {1}".format(n, get_roku_info(roku.get("location"))))
+            n = n + 1
+        sys.exit(0)
+
+    roku = ""
+    if len(args.url) > 0:
+        roku = args.url
+    else:
+        rokus = get_roku()
+        if (len(rokus) > 1) and (args.number == 0):
+            n = 1
+            for roku in rokus:
+                print("{0} - {1}".format(n, get_roku_info(roku.get("location"))))
+                n = n + 1
+            num = input("Enter number")
+            roku = rokus[num - 1].get("location")
+        elif (len(rokus) > 1) and (args.number != 0):
+            roku = rokus[args.number - 1].get("location")
+        else:
+            roku = rokus[0].get("location")
+
+    URL = args.video
+
+    ydl_opts = {'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(URL, download=False)
+        jinfo = ydl.sanitize_info(info)
+        res = pick_format(jinfo)
+        if res == None:
+            print("No usable format found")
+            sys.exit(1)
+        requests.post(make_roku_url(roku, res['url']))
+
+main()
